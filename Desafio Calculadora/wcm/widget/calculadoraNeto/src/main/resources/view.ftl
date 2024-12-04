ac507a423688bede79f7b247d9473890771dd8ec
NetoPetit <cidao_petit5@hotmail.com>
2024-12-04 19:31:51 -0300

    Calculadora

--- Desafio Calculadora/wcm/widget/calculadoraNeto/src/main/resources/view.ftl
+++ Desafio Calculadora/wcm/widget/calculadoraNeto/src/main/resources/view.ftl
@@ -1,4 +1,37 @@
 <div id="MyWidget_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide" data-params="MyWidget.instance()">
-	<p>Teste de exercício</p>
+	<div class="panel panel-primary">
+    	
+    	<div class="panel-heading">
+        	<h3 class="panel-title">${i18n.getTranslation("view.titulo")}</h3>
+    	</div>
+    	
+    	<div class="panel-body">
+    		<div class="row">
+       	 		<form class="form-inline" role="form">
+					<div class="form-group">
+				        <label for="exampleInputCalculadora">${i18n.getTranslation("view.v1")}</label>
+				        <input type="number" class="form-control" id="exampleInputCalculadora1_${instanceId}" placeholder="Insira um valor">
+				    </div>
+				    <div class="form-group">
+				        <label for="exampleInputCalculadora">${i18n.getTranslation("view.v2")}</label>
+				        <input type="number" class="form-control" id="exampleInputCalculadora2_${instanceId}" placeholder="Insira um valor">
+				    </div>
+				</form>
+				<br />
+				    <div id="buttons">
+				    	<button type="button" class="btn btn-primary" data-somar>${i18n.getTranslation("view.add")}</button>	
+				    	<button type="button" class="btn btn-primary" data-subtrair>${i18n.getTranslation("view.sub")}</button>			
+				    	<button type="button" class="btn btn-primary" data-multiplicar>${i18n.getTranslation("view.mult")}</button>			
+				    	<button type="button" class="btn btn-primary" data-dividir>${i18n.getTranslation("view.div")}</button>			
+				    </div>
+				    <br />
+				    <div class="form-group">
+				        <label for="exampleInputCalculadora" id="resultado">${i18n.getTranslation("view.result")}</label>
+				        <input type="text" class="form-control" id="exampleInputCalculadora3_${instanceId}" placeholder="Resultado" readonly>
+					</div>
+			</div>
+   		</div>
+	
+	</div>
 </div>
 
--- Desafio Calculadora/wcm/widget/calculadoraNeto/src/main/webapp/resources/js/calculadoraNeto.js
+++ Desafio Calculadora/wcm/widget/calculadoraNeto/src/main/webapp/resources/js/calculadoraNeto.js
@@ -6,17 +6,43 @@ var MyWidget = SuperWidget.extend({
     //método iniciado quando a widget é carregada
     init: function() {
     },
-  
-    //BIND de eventos
+    
+     //BIND de eventos
     bindings: {
         local: {
-            'execute': ['click_executeAction']
-        },
+            'somar': ['click_executaSoma'],
+    		'subtrair': ['click_executaSubtrair'],
+    		'multiplicar': ['click_executaMultiplicar'],
+    		'dividir': ['click_executaDividir']
+     },
+     
         global: {}
     },
  
-    executeAction: function(htmlElement, event) {
+    executaSoma: function() {
+    	var valor1 = parseInt($("#exampleInputCalculadora1_" + this.instanceId).val(), 10);
+        var valor2 = parseInt($("#exampleInputCalculadora2_" + this.instanceId).val(), 10);
+    	var soma = valor1 + valor2;
+    	$("#exampleInputCalculadora3_"+this.instanceId).val(soma);
+    },
+    executaSubtrair: function() {
+    	var valor1 = parseInt($("#exampleInputCalculadora1_" + this.instanceId).val(), 10);
+        var valor2 = parseInt($("#exampleInputCalculadora2_" + this.instanceId).val(), 10);
+    	var subtracao = valor1 - valor2;
+    	$("#exampleInputCalculadora3_"+this.instanceId).val(subtracao);
+    },
+    executaMultiplicar: function() {
+    	var valor1 = parseInt($("#exampleInputCalculadora1_" + this.instanceId).val(), 10);
+        var valor2 = parseInt($("#exampleInputCalculadora2_" + this.instanceId).val(), 10);
+    	var multiplicacao = valor1 * valor2;
+    	$("#exampleInputCalculadora3_"+this.instanceId).val(multiplicacao);
+    },
+    executaDividir: function() {
+        var valor1 = parseInt($("#exampleInputCalculadora1_" + this.instanceId).val(), 10);
+        var valor2 = parseInt($("#exampleInputCalculadora2_" + this.instanceId).val(), 10);
+        var divisao = valor1 / valor2;
+    	$("#exampleInputCalculadora3_"+this.instanceId).val(divisao);
     }
-
+  
 });
 
