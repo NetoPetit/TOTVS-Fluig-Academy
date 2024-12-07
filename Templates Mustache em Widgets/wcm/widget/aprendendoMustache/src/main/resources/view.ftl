13e28755b77a7f4c6a2c6e1c09cde1f231174c99
NetoPetit <cidao_petit5@hotmail.com>
2024-12-06 21:03:07 -0300

    mustache

--- Templates Mustache em Widgets/wcm/widget/aprendendoMustache/src/main/resources/view.ftl
+++ Templates Mustache em Widgets/wcm/widget/aprendendoMustache/src/main/resources/view.ftl
@@ -1,6 +1,6 @@
 <div id="MyWidget_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide" data-params="MyWidget.instance()">
 	
-	<script id="meuPrimeiroTemplate" class="text/template">
+	<script id="meuPrimeiroTemplate" type="text/template">
 		<div class="row">
 			<div class="col-md-3">
 			{{title}} spends {{calc}}
@@ -8,7 +8,57 @@
 		</div>
 	</script>
 	
+	<script id="pessoasTemplate" type="text/template">
+		<div class="row">
+			
+			{{#pessoas}}
+			<div class="col-md-3">
+				<p>O(a) {{nome}} possui {{idade}} anos de idade.</p>
+			</div>
+			{{/pessoas}}
+			
+		</div>
+	</script>
+	
+	<script id="produtosTemplate" type="text/template">
+		
+		<div class="row">
+			
+			{{#produtos}}
+			<div class="col-md-4">
+		        <div class="card">
+		            <img class="card-img-top" src={{icone}} alt="Card image cap">
+		            <div class="card-body">
+		                <h3 class="card-title">{{nome}}</h3>
+		                <p class="card-text">{{descricao}}</p>
+		                <a href="#" class="btn btn-primary">Ver mais</a>
+		                <p class="card-text">Avaliações</p>
+		                {{#avaliacao}}
+		                	<p class="card-text">Avaliação feita por {{autor}}: {{conteudo}}</p>
+		                {{/avaliacao}}
+		                
+		                {{^avaliacao}}
+		                	<p class="card-text">Sem avaliações</p>
+		                {{/avaliacao}}
+		                
+		            </div>
+		        </div>
+	    	</div>
+	    	{{/produtos}}
+	    	
+		</div>
+	
+	</script>
+	
+	<h2>Exemplo 1:</h2>
 	<div data-meuPrimeiroTemplate></div>
+	
+	<h2>Exemplo 2:</h2>
+	<div data-pessoasTemplate></div>
+	
+	<h2>Exemplo 3:</h2>
+	<div data-produtosTemplate></div>
+	
 	
 </div>
 
--- Templates Mustache em Widgets/wcm/widget/aprendendoMustache/src/main/webapp/resources/js/aprendendoMustache.js
+++ Templates Mustache em Widgets/wcm/widget/aprendendoMustache/src/main/webapp/resources/js/aprendendoMustache.js
@@ -6,6 +6,9 @@ var MyWidget = SuperWidget.extend({
     //método iniciado quando a widget é carregada
     init: function() {
     	this.exemploMustache1();
+    	this.exemploMustache2();
+    	this.exemploMustache3();
+
     },
   
     //BIND de eventos
@@ -14,7 +17,9 @@ var MyWidget = SuperWidget.extend({
     	local: {
             'execute': ['click_executeAction']
         },
-        global: {}
+        global: {
+        	
+        }
     
     },
     
@@ -30,7 +35,54 @@ var MyWidget = SuperWidget.extend({
     	var template = document.getElementById("meuPrimeiroTemplate").innerHTML;
     	const output = Mustache.render(template , view);
     	$("[data-meuPrimeiroTemplate]").append(output);
-    }
+    },
+    exemploMustache2: function() {
+    	
+    	const view = {
+    			pessoas: [
+    			          	{"nome": "Luis", "idade": "19"},
+    			          	{"nome": "Ana", "idade": "20"},
+    			          	{"nome": "João", "idade": "21"}
+    			          ]
+    	};
+    	
+    	var template = document.getElementById("pessoasTemplate").innerHTML;
+    	const output = Mustache.render(template , view);
+    	$("[data-pessoasTemplate]").append(output);
+    },
+	exemploMustache3: function() {
+		
+		const view = {
+				produtos: [
+				           	{
+				           		"nome": "Produto A",
+				           		"descricao": "aqui vai a descricao do produto A",
+				           		"preco": "R$ 112,00",
+				           		"icone": "/aprendendoMustache/resources/images/clientimg.png",
+				           		"avaliacao": [
+				           		              	{"autor": "Cliente 1", "conteudo": "Descricao da avaliacao 1"},
+				           		              	{"autor": "Cliente 2", "conteudo": "Descricao da avaliacao 2"},
+				           		              	{"autor": "Cliente 3", "conteudo": "Descricao da avaliacao 3"},
+				           		              	{"autor": "Cliente 4", "conteudo": "Descricao da avaliacao 4"}
+				           		              ]
+				           	},
+				           	{
+				           		"nome": "Produto B",
+				           		"descricao": "aqui vai a descricao do produto B",
+				           		"preco": "R$ 150,00",
+				           		"icone": "/aprendendoMustache/resources/images/clientimg.png",
+				           		"avaliacao": [ ]
+				           	}
+				           ]
+		
+		}
+		
+		var template = document.getElementById("produtosTemplate").innerHTML;
+    	const output = Mustache.render(template , view);
+    	$("[data-produtosTemplate]").append(output);
+		
+	}
+
    
 });
 
