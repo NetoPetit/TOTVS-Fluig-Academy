4a38971c916587b4976994a737aeea5409230330
NetoPetit <cidao_petit5@hotmail.com>
2025-01-09 18:56:54 -0300

    Atualizacao Desenvolvimento BPM/ECM Advanced

--- Desenvolvimento de Processos_BPM_ECM Advanced/forms/Banco de Talentos/events/displayFields.js
+++ Desenvolvimento de Processos_BPM_ECM Advanced/forms/Banco de Talentos/events/displayFields.js
@@ -5,9 +5,12 @@ function displayFields(form,customHTML){
 	var linkedin = form.getValue("linkedin");
 	var email = form.getValue("email");
 	
-	var interacao = 'Oi ' + usuario + '. O ' + nome + ' deseja trabalhar conosco!' +
-	'O seu perfil é: ' + perfil + '. E você pode conferir seu currículo no Linkedin: ' +  linkedin + '.' + 
-	'Você também pode entrar em contato pelo e-mail: ' + email + '.' + 
-	'Obrigado!'
+	var interacao = '<h2>Oi, <b>' + usuario + '</b>. O <b>' + nome + '</b> deseja trabalhar conosco!</h2><br>' +
+	'<h3>O perfil dele é: <b>' + perfil + '</b>. E você pode conferir o currículo dele no Linkedin: <b>' +  linkedin + '</b>.<br>' + 
+	'Você também pode entrar em contato com ele pelo e-mail: <b>' + email + '</	b>.<br>' + 
+	'Obrigado!</h3>';
+	
+	customHTML.append('<script>$("#mensagemInteracao").append("'+interacao+'")</script>');
+	customHTML.append('<script>$("#mensagemInteracao").show();$("#formPrincipal").hide();</script>');
 	
 }
