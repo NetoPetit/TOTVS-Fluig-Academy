d0dcef14cd538a487ac8c481a9c57f53234e83e0
NetoPetit <cidao_petit5@hotmail.com>
2025-01-11 17:07:57 -0300

    Atualizacao Desenvolvimento BPM/ECM Advanced

---
+++ Desenvolvimento de Processos_BPM_ECM Advanced/events/Lab/displayCentralTasks.js
@@ -0,0 +1,6 @@
+function displayCentralTasks(links) {
+	
+	links.put('Domine a Central de Tarefas', "https://lab.fluig.com/services");
+	links.put('Fluig Developer', "https://style.fluig.com");
+	
+}
--- Desenvolvimento de Processos_BPM_ECM Advanced/forms/Avaliar Candidato/criaUsuario.js
+++ Desenvolvimento de Processos_BPM_ECM Advanced/forms/Avaliar Candidato/criaUsuario.js
@@ -1,9 +1,17 @@
-var c1 = DatasetFactory.createConstraint("colleagueId", $("#login").val(), $("#login").val(), ConstraintType.MUST);
-var c2 = DatasetFactory.createConstraint("colleagueName", $("#nome").val(), $("#nome").val(), ConstraintType.MUST);
-var c3 = DatasetFactory.createConstraint("login", $("#login").val(), $("#login").val(), ConstraintType.MUST);
-var c4 = DatasetFactory.createConstraint("mail", $("#email").val(), $("#email").val(), ConstraintType.MUST);
-var c5 = DatasetFactory.createConstraint("passwd", $("#senha").val(), $("#senha").val(), ConstraintType.MUST);
+$("#criaUsuario").click( function() {
+	var c1 = DatasetFactory.createConstraint("colleagueId", $("#login").val(), $("#login").val(), ConstraintType.MUST);
+	var c2 = DatasetFactory.createConstraint("colleagueName", $("#nome").val(), $("#nome").val(), ConstraintType.MUST);
+	var c3 = DatasetFactory.createConstraint("login", $("#login").val(), $("#login").val(), ConstraintType.MUST);
+	var c4 = DatasetFactory.createConstraint("mail", $("#email").val(), $("#email").val(), ConstraintType.MUST);
+	var c5 = DatasetFactory.createConstraint("passwd", $("#senha").val(), $("#senha").val(), ConstraintType.MUST);
 
-var constraints = new Array(c1, c2, c3, c4, c5);
+	var constraints = new Array(c1, c2, c3, c4, c5);
 
-DatasetFactory.getDataset("criaUser", null, constraint, null);
+	DatasetFactory.getDataset("criaUser", null, constraint, null);
+	
+	FLUIG.toast({
+		message: 'Login para ' + $("#nome").val() + ' criado!' ,
+		type: 'info'
+	})
+});
+
